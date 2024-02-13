5f58b3e1c066aac90d28db2c8b8411b716893f25
Geir Sagberg <geir.sagberg@gmail.com>
2024-02-13 13:03:14 +0100

    Update to bevy 0.12

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
-use bevy_pixel_camera::{PixelCameraBundle, PixelCameraPlugin, PixelProjection};
+use bevy_pixel_camera::{PixelCameraBundle, PixelCameraPlugin, PixelProjection, PixelZoom};
 
 fn main() {
     App::new()
@@ -8,32 +8,28 @@ fn main() {
             DefaultPlugins
                 .set(ImagePlugin::default_nearest())
                 .set(WindowPlugin {
-                    window: WindowDescriptor {
-                        width: 512.,
-                        height: 512.,
+                    primary_window: Some(Window {
+                        title: "Green Quest".to_string(),
+                        resolution: (512., 512.).into(),
                         resizable: false,
                         ..default()
-                    },
+                    }),
                     ..default()
                 }),
         )
-        .add_plugin(PixelCameraPlugin)
-        .add_plugin(LdtkPlugin)
-        .insert_resource(LevelSelection::Index(0))
-        .add_startup_system(setup)
-        .add_system(place_monstera_on_click)
+        .add_plugins(PixelCameraPlugin)
+        .add_plugins(LdtkPlugin)
+        .insert_resource(LevelSelection::Uid(0))
+        .add_systems(Startup, setup)
+        .add_systems(Update, place_monstera_on_click)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let mut camera = PixelCameraBundle::new(PixelProjection {
-        zoom: 2,
-        centered: false,
-        ..Default::default()
-    });
-    commands.spawn(camera);
+    commands.spawn((Camera2dBundle::default(), PixelZoom::Fixed(2)));
     commands.spawn(LdtkWorldBundle {
         ldtk_handle: asset_server.load("graphics/house.ldtk"),
+        transform: Transform::from_translation(Vec3::new(-128., -128., 0.)),
         ..default()
     });
 }
@@ -42,23 +38,21 @@ fn place_monstera_on_click(
     mut commands: Commands,
     mouse_button_input: Res<Input<MouseButton>>,
     asset_server: Res<AssetServer>,
-    camera_query: Query<(&Transform, &Camera)>,
-    windows: Res<Windows>,
+    camera_query: Query<(&GlobalTransform, &Camera)>,
+    window_query: Query<&Window>,
 ) {
-    let (camera_transform, camera) = camera_query.single();
-
-    let window = windows.get_primary().unwrap();
-
     if mouse_button_input.just_pressed(MouseButton::Left) {
-        let position = window.cursor_position().unwrap();
+        let (camera_transform, camera) = camera_query.single();
+        let window = window_query.single();
+        let cursor_position = window.cursor_position().unwrap();
+
+        let point = camera
+            .viewport_to_world_2d(camera_transform, cursor_position)
+            .unwrap();
 
         commands.spawn(SpriteBundle {
             texture: asset_server.load("graphics/monstera2.png"),
-            transform: Transform::from_translation(Vec3::new(
-                position.x / 2. - 1.,
-                position.y / 2. + 12.,
-                3.,
-            )),
+            transform: Transform::from_translation(Vec3::new(point.x - 1., point.y + 12., 10.)),
             ..default()
         });
     }
